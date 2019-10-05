8a8bfb8a680bd283b4133b8f71d38bb5159455d8
Luiz Miguel <luizmiguel.oliveira18@gmail.com>
2019-10-05 20:06:50 -0300

    Removing comment

--- backend/src/gerartable.js
+++ backend/src/gerartable.js
@@ -2,11 +2,3 @@ const Carro = require('./models/carros');
 
 
 Carro.sync({force:true});
-/*Carro.create({
-	veiculo: "gabriel",
-	marca: "x",
-	ano: "200",
-	descricao: "text",
-	vendido: false,
-	
-})*/
